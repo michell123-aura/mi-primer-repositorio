e5e0738add319ff68cc62acf822c3304479bfe4a
michell123-aura <michelloviedo80@gmail.com>
2026-04-26 16:51:46 -0500

    Create Variables.py for variable hand
    Add a Python script for variable initialization and user input.

---
+++ Variables.py
@@ -0,0 +1,16 @@
+#Inicializando variables y colocandoles un valor
+nombre= "Michell Alexandra Oviedo Vasquez"
+edad= 13
+Estado= True
+
+#Esta linea me imprime el contenido de una variable
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre? ")
+edad=input("¿Cual es tu edad? ")
+
+#Mostrar el resultado capturado con input()
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
